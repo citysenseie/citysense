129f9a24302a40d6426b89728c7f6df9f2eb5db2
Ebrim <ebrim@example.com>
2026-05-30 16:32:06 +0200

    Add Firebase Storage setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,29 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, addDoc, getDocs, query, orderBy, serverTimestamp, GeoPoint, Timestamp } from "firebase/firestore";
-
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  serverTimestamp,
+  GeoPoint,
+  Timestamp,
+} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyDfydHxczoAw-xs8Rc2fP0KsarPB3DmVSE",
   authDomain: "citysense-3e7a4.firebaseapp.com",
   projectId: "citysense-3e7a4",
   storageBucket: "citysense-3e7a4.firebasestorage.app",
   messagingSenderId: "653017701073",
-  appId: "1:653017701073:web:9101c90467e4bddf7843f0"
+  appId: "1:653017701073:web:9101c90467e4bddf7843f0",
 };
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export { collection, addDoc, getDocs, query, orderBy, serverTimestamp, GeoPoint, Timestamp };
